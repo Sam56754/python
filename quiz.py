9a9b8ff258c2c7ee87e3e77df6b6d16dc3916f02
Sam56754 <sam01candor@gmail.com>
2025-01-24 23:31:21 +0300

    updated code

--- quiz.py
+++
@@ -1,3 +0,0 @@
-#radius input and calculates the volume 4/3pi r 3  use math library for pi and ans in 2dp#
-radius = float(input("Enter the radius: "))
-volume = 4/3
---
+++ volume.py
@@ -0,0 +1,7 @@
+#radius input and calculates the volume 4/3pi r 3  use math library for pi and ans in 2dp#
+import math
+
+radius = float(input("Enter the radius of the sphere: "))
+volume = (4/3 )* math.pi * math.pow(radius,3)
+
+print(f"the volume o fthe sphere is {volume:.2f}")

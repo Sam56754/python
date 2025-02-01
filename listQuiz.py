8c202b9bff4b4a424dedf09b0a17f801c968b6be
Sam56754 <sam01candor@gmail.com>
2025-02-01 19:59:33 +0300

    lists update

--- listQuiz.py
+++ listQuiz.py
@@ -1,4 +1,8 @@
 # program to perform operations on a lis of input from user
 
-names = input("Enter the names: ")
-
+names = input("Enter the names: ").split()
+sorted_names = sorted(names) # sort the names alphabetically
+print("names list: ")
+for name in sorted_names:
+    print(name) 
+print("The total number of names is: ", len(sorted_names)) #displays the list size

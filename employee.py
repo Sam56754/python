efa4e6ae914c2a259fe08172741347529e683676
Sam56754 <sam01candor@gmail.com>
2025-03-07 23:59:02 +0300

    updates ii

--- employee.py
+++ employee.py
@@ -1,35 +1,34 @@
 # employee service program
 
-class employee:
-    def __init__(self, emp_id, name, salary):
+class Employee:
+    def __init__(self, emp_id, name):
         self.emp_id = emp_id
         self.name = name
-        self.salary = salary
+        self.salary = 0
         
-    def calculate_salary():
+    def calculate_salary(self):
         return self.salary
     
-class HourlyEmployee(employee):
-    def __init__(self, hourly_rate, hours_worked):
-        super().__init__(emp_id, name, salary)
+class HourlyEmployee(Employee):
+    def __init__(self, emp_id, name, hourly_rate, hours_worked):
+        super().__init__(emp_id, name)
         self.hourly_rate = hourly_rate
         self.hours_worked = hours_worked
     
     def calculate_salary(self):
-        return f"{self.hourly_rate * self.hours_worked}"
+        return f"{self.name} earns Ksh {self.hourly_rate * self.hours_worked} working {self.hours_worked}hrs"
     
-class salaried_employee(employee):
-    def __init__(self, monthly_salary):
-        super().__init__(emp_id, name, salary)
+class SalariedEmployee(Employee):
+    def __init__(self, emp_id, name, monthly_salary):
+        super().__init__(emp_id, name)
         self.monthly_salary = monthly_salary
         
-    def calculate_salary():
-        salary = self.monthly_salary
-        return salary
+    def calculate_salary(self):
+        self.salary = self.monthly_salary
+        return f"{self.name} is a monthly salaried employee earning Ksh {self.salary}"
 
-person = employee(12444567, "mark", 5000)
-person_1 = HourlyEmployee(500, 8)
-person_2 = salaried_employee(30000)
+person_1 = HourlyEmployee(127843, "john", 500, 8)
+person_2 = SalariedEmployee(8969973, "mary", 30000)
 
-print(john.calculate_salary())
-print(sam.calculate_salary())
+print(person_1.calculate_salary())
+print(person_2.calculate_salary())
